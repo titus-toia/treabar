4e5bb1960842a3432876771d736fe7dfd1062934
Titus Toia <titustoia@gmail.com>
2015-12-11 20:20:34 +0200

    First commit; work already started on first page

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('main')
+
+@section('content')
+<div id="dashboard" class="row">
+  <div class="large-8 columns main scrollpanel">
+    Dashboard main
+  </div>
+  <aside class="large-4 columns notifications">
+    <ul class="tabs" data-tab>
+      <li class="tab-title active"><a href="#discussion-panel">DISCUSSION<div class="tab-pedestal"></div></a></li>
+      <li class="tab-title"><a href="#activity-panel">ACTIVITY<div class="tab-pedestal"></div></a></li>
+    </ul>
+    <div class="tabs-content">
+      <div class="content active" id="discussion-panel">
+        <p>This is the first panel of the basic tab example. You can place all sorts of content here including a grid.</p>
+      </div>
+      <div class="content" id="activity-panel">
+        <p>This is the second panel of the basic tab example. This is the second panel of the basic tab example.</p>
+      </div>
+    </div>
+  </aside>
+</div>
+@endsection
---
+++ resources/views/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Treabar</title>
+
+  <!-- Fonts -->
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+  <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css">
+
+  <link href="{{ asset('foundation/css/foundation.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('foundation/icons/foundation-icons.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/foundation-amends.css') }}" rel="stylesheet" type="text/css">
+  <script src="{{ asset('foundation/js/vendor/modernizr.js') }}"></script>
+</head>
+<body>
+<nav class="top-bar">
+  <ul class="title-area">
+    <li class="name"><!-- Leave this empty --></li>
+  </ul>
+  <section class="top-bar-section">
+    <ul class="left">
+      <li class="dashboard page"><a href="#">Dashboard</a></li>
+      <li class="manager page"><a href="#">Manage</a></li>
+      <li class="invoices page"><a href="#">Invoice</a></li>
+    </ul>
+    <ul class="right">
+      <li class="fullname"><span>Full Name</span></li>
+      <li class="has-dropdown profile">
+        <a href="#"><i class="fi-torso"></i></a>
+        <ul class="dropdown">
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Log Out</a></li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</nav>
+
+@yield('content')
+<script src="{{ asset('foundation/js/vendor/jquery.js') }}"></script>
+<script src="{{ asset('foundation/js/foundation.min.js') }}"></script>
+<script src="{{ asset('js/jquery.mousewheel.js') }}"></script>
+<script src="{{ asset('js/jquery.scrollpanel.js') }}"></script>
+<script>
+  $(document).foundation();
+  $.ready(function() {
+    $('.scrollpanel').scrollpanel();
+  });
+</script>
+</body>
+</html>
