ad6e9820f2cabbb3736ad02c15f53832d9a76934
unknown <titus@etanova.com>
2015-12-11 20:52:50 +0200

    merging from laptop

---
+++ resources/views/manage.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+@section('content')
+<div id="manage">
+  <ul class="lateral-tabs">
+    <li id="manage-tasks">TASKS</li>
+    <li id="manage-timesheet">TIMESHEET</li>
+    <li id="manage-chart">CHART</li>
+    <li id="manage-feed">FEED</li>
+  </ul>
+</div>
+@endsection
