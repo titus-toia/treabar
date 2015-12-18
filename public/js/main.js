037735bd21b8d73e7396708f2a311023772682b1
Titus Toia <titustoia@gmail.com>
2015-12-18 18:53:44 +0200

    changes on project manager scrolling

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ $('body').on('click', '#manager-tabs div', function() {
 });
 
 
+
 $(document).ready(function() {
   var $frame = $('#manager-projects-list-wrapper');
   var $scrollbar = $frame.parent().find('.scrollbar');
@@ -25,9 +26,23 @@ $(document).ready(function() {
     dragHandle: 1,
     dynamicHandle: 1,
     clickBar: 1
+  }, {
+    active: function(ev, pos) {
+      setTimeout(function() {
+        $frame.find('.slidee > li > div.manager-project').css('border-width', '2px');
+        var $project = $frame.find('.slidee > li:nth(' + pos + ') > div.manager-project');
+        var $bridge = $('#bridge');
 
-    // Buttons
-    //prev: $wrap.find('.prev'),
-    //next: $wrap.find('.next')
+        $project.css('border-width', '2px 2px 0px 2px');
+        var offset = $project.position();
+        $bridge[0].style.top = (offset.top + $project.outerHeight() + 5) + 'px';
+        $bridge[0].style.left = offset.left + 'px';
+        $bridge.show(200);
+      }, 400);
+    },
+    moveStart: function(ev) {
+      $frame.find('.slidee > li > div.manager-project').css('border-width', '2px');
+      $('#bridge').hide();
+    }
   });
 });
--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -2,36 +2,43 @@
 @section('content')
 <div id="manage" class="row wrapper">
   <div id="manager-tabs" class="columns large-1">
-      <div id="manage-tasks"><a href="#tasks">TASKS</a></div>
-      <div id="manage-timesheet"><a href="#timesheet">TIMESHEET</a></div>
-      <div id="manage-chart"><a href="#chart">CHART</a></div>
-      <div id="manage-feed"><a href="#feed">FEED</a></div>
+    <div id="manage-tasks"><a href="#tasks">TASKS</a></div>
+    <div id="manage-timesheet"><a href="#timesheet">TIMESHEET</a></div>
+    <div id="manage-chart"><a href="#chart">CHART</a></div>
+    <div id="manage-feed"><a href="#feed">FEED</a></div>
   </div>
   <div id="manager-projects" class="columns large-11">
     <div id="manager-projects-list-wrapper" class="frame">
       <ul id="manager-projects-list" class="slidee">
-        @for($i = 0; $i < 10; $i++)
-        <li>
-          <div class="manager-project">
-            <div class="left">
-              <span class="name">Project Name</span>
-              <span class="date">{{ date('Y-m-d', strtotime('-1 month'))  }}</span>
-              <ul class="people">
-                @for($keys = array_rand(range(1, 7), rand(2, 4)), $j = 0; $j < count($keys); $j++)
-                <li><img height="35" width="35" src="{{ url('images/dev/user-' . range(1, 7)[$keys[$j]] . '.jpg') }}" /></li>
-                @endfor
-              </ul>
-              <div class="clearfix"></div>
-              <span class="tracked">Tracked {{ rand(10, 150) }} hours.</span>
+        @for($i = 1; $i <= 10; $i++)
+          <li>
+            <div class="manager-project" data-id="{{ $i }}">
+              <div class="left">
+                <span class="name">Project Name</span>
+                <span class="date">{{ date('Y-m-d', strtotime('-1 month'))  }}</span>
+                <ul class="people">
+                  @for($keys = array_rand(range(1, 7), rand(2, 4)), $j = 0; $j < count($keys); $j++)
+                    <li><img height="35" width="35"
+                             src="{{ url('images/dev/user-' . range(1, 7)[$keys[$j]] . '.jpg') }}"/></li>
+                  @endfor
+                </ul>
+                <div class="clearfix"></div>
+                <span class="tracked">Tracked {{ rand(10, 150) }} hours.</span>
+              </div>
+              <div class="banner color-{{ rand(1, 5) }}"></div>
             </div>
-            <div class="banner color-{{ rand(1, 5) }}"></div>
-          </div>
-        </li>
+          </li>
         @endfor
       </ul>
     </div>
-    <div class="scrollbar">
-      <div class="handle"></div>
+    <div class="scrollbar"><div class="handle"></div></div>
+    <div id="bridge"></div>
+    <div id="current-project" class="columns large-12">Project Info
+      @for($i = 1; $i <= 10; $i++)
+      <div class="current-project-listing" data-id="{{ $id }}">
+        Stuff about this project
+      </div>
+      @endfor
     </div>
   </div>
 </div>
