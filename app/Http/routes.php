f5edaa13738d4f881f4327e0bb00fff4331d0ca7
Titus Toia <titustoia@gmail.com>
2016-01-02 14:15:18 +0200

    Huge migration to Laravel 5.2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,26 +2,35 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-  'auth' => 'Auth\AuthController',
-  'password' => 'Auth\PasswordController',
-]);
-
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
 Route::get('manage', 'ManagerController@index');
 Route::get('manage/{id}/tasks', 'ManagerController@tasks');
 Route::get('manage/{id}/timesheet', 'ManagerController@timesheet');
 Route::get('manage/{id}/chart', 'ManagerController@chart');
 Route::get('manage/{id}/feed', 'ManagerController@feed');
+
+Route::group(['middleware' => ['web']], function () {
+    //
+});
