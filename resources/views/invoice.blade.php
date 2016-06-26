36e9407a0b55995ef253c046f5e87054bcf896b0
Titus Toia <titustoia@gmail.com>
2016-06-26 14:49:20 +0300

    invoice listing style

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -17,19 +17,38 @@
       width: 150px;
       margin-right: 20px;
       margin-bottom: 10px;
+      border: 2px solid whitesmoke;
+      border-left-width: 0;
       background-color: whitesmoke;
       cursor: pointer;
     }
     #invoice-list .invoice .banner {
       width: 10px;
-      height: 140px;
+      height: calc(100% - 15px);
       float: left;
     }
     #invoice-list .invoice .preview {
+      padding: 3px;
       overflow: hidden;
-      height: 140px;
-      width: 140px;
+      height: calc(100% - 15px);
+      width: calc(100% - 10px);
       white-space: nowrap;
+    }
+    #invoice-list .invoice .date {
+      float: right;
+      display: inline-block;
+      font-size: 10px;
+      text-align: right;
+    }
+    #invoice-list .invoice dl {
+      font-size: 11px;
+    }
+
+    #invoice-list .invoice dt {
+      margin-bottom: 3px;
+    }
+    #invoice-list .invoice dd {
+      margin-bottom: 6px;
     }
   </style>
   <div id="invoice" class="row wrapper">
@@ -39,8 +58,15 @@
         <div class="invoice">
           <div class="banner color-{{ $invoice->project->color }}" data-color="color-{{ $invoice->project->color }}"></div>
           <div class="preview">
-            <span class="hours">{{ $invoice->total()['hours'] }}h</span>
-            <span class="total">${{ $invoice->total()['total'] }}</span>
+            <span class="date">{{ $invoice->issued_at  }}</span>
+            <dl>
+              <dt>Client</dt>
+              <dd>{{ $invoice->client }}</dd>
+              <dt>Hours</dt>
+              <dd class="hours">{{ $invoice->total()['hours'] }}h</dd>
+              <dt>Amount billed</dt>
+              <dd class="total">${{ $invoice->total()['total'] }}</dd>
+            </dl>
           </div>
           <span class="name caption"></span>
         </div>
